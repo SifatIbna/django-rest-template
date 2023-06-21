cf8bc00d25447665ad7b6b322101c41d8bcecdb3
shifu <sifatibna.amin9@gmail.com>
2023-06-21 11:58:58 +0600

    added user authentication and verification

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .apiviews import PollList,PollDetail
+from .apiviews import PollList,PollDetail,ChoiceList,CreateVote,UserCreate,LoginView
 
 urlpatterns = [
+    path("login/",LoginView.as_view(),name="login"),
+    path("users/",UserCreate.as_view(),name="user_create"),
     path("polls/", PollList.as_view(), name="polls_list"),
-    path("polls/<int:pk>/", PollDetail.as_view(), name="polls_detail")
+    path("polls/<int:pk>/", PollDetail.as_view(), name="polls_detail"),
+    path("polls/<int:pk>/choices/",ChoiceList.as_view(),name="choice_list"),
+    path("polls/<int:pk>/choices/<int:choice_pk>/vote/",CreateVote.as_view(),name="create_vote"),
+    path("vote/",CreateVote.as_view(),name="create_vote")
 ]
